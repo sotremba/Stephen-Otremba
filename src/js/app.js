7473bd798dd029924d10834ccc1cacf9afaeaf4d
Stephen Otremba <40135048+sotremba@users.noreply.github.com>
2020-05-13 21:30:27 -0700

    color change

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#5cbdaa"
       },
       "shape": {
         "type": "polygon",
